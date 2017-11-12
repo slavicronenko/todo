4cfc95752b8e243e18142cba73800f1333ae0faa
viacheslav.ronenko <slavic.ronenko@gmail.com>
2017-11-12 20:45:19 +0200

    Preparing the working directory.

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.scss';
+import ToDoList from './ToDoList';
+import {
+  Grid,
+  Row,
+  Col
+} from 'react-bootstrap';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Grid>
+          <Row className="show-grid">
+            <Col xs={12}>
+              <ToDoList></ToDoList>
+            </Col>
+          </Row>
+        </Grid>
       </div>
     );
   }
---
+++ src/ToDoList/ToDoList.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+import './ToDoList.scss';
+
+class ToDoList extends Component {
+  render() {
+    return (
+      <ul className="ToDoList">
+        <li>1</li>
+        <li>2</li>
+        <li>3</li>
+        <li>4</li>
+        <li>5</li>
+      </ul>
+    );
+  }
+}
+
+export default ToDoList;
