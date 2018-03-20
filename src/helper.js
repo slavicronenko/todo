ae5db72e9f822806418a28374c8f341c110ad625
viacheslav.ronenko <slavic.ronenko@gmail.com>
2018-03-21 00:37:33 +0200

    Feature: Add filterWithConditions method to the helper

--- src/helper.js
+++ src/helper.js
@@ -6,5 +6,9 @@ export function getNextIndex(list, indexProp = 'id') {
   return list.reduce((currentIndex, item) => item[indexProp] >= currentIndex ? item[indexProp] : currentIndex, 0) + 1;
 }
 
+export function filterWithConditions (collection, conditions, matchAll = true) {
+  return collection.filter((...args) => conditions.reduce((result, condition) => condition(...args) && (!matchAll || result), true));
+}
+
 export const ESCAPE_KEY = 27;
 export const ENTER_KEY = 13;
